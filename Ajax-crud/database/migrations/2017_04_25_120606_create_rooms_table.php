9b6f5ab18d3b3e3df09c9c3b8209caa75abae800
Andreas <aks1992@gmail.com>
2017-04-25 14:23:52 +0200

    legg til rom i databasen

---
+++ Ajax-crud/database/migrations/2017_04_25_120606_create_rooms_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use App\Room; 
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('body');
+            $table->timestamps();
+        });
+
+        $room = new Room;
+        $room->body = 'Tangen';
+        $room->save();
+
+        $room2 = new Room;
+        $room2->body = 'Corneren';
+        $room2->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
